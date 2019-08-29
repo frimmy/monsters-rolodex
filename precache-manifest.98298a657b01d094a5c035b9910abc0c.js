40941cffc750e03437108e4fdb03ab9626a0c981
frimmy-ww <adrian.frimpong@weightwatchers.com>
2019-08-29 00:36:09 -0400

    Updates

---
+++ precache-manifest.98298a657b01d094a5c035b9910abc0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f586e7a540885649196696bd5b3ce131",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "37e5e985d15a55ff6bf2",
+    "url": "/monsters-rolodex/static/css/main.c22bb501.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "37e5e985d15a55ff6bf2",
+    "url": "/monsters-rolodex/static/js/main.9ef150c4.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
